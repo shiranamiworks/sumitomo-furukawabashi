ae8b5a6b4a09b53eebf6ccacef1bbe917881661d
福井彩佳 <ayaka.fsp@gmail.com>
2025-01-23 17:23:18 +0900

    no message

---
+++ develop/js/private.js
@@ -0,0 +1,21 @@
+
+$(function () {
+  $(".slider").slick({
+    autoplay: true,
+    arrows: true,
+    fade: true,
+    asNavFor: ".thumbnail",
+  });
+  $(".thumbnail").slick({
+    slidesToShow: 10,
+    asNavFor: ".slider",
+    focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                slidesToShow: 3,
+            }
+        ]
+  });
+});
+
---
+++ develop/private.php
@@ -0,0 +1,164 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "private",
+  "gnav_set" => "private",
+  "page_class" => "private",
+  "url" => "private.html",
+  "title" => "室内空間｜",
+  "keywords" => "室内空間,",
+  "description" => "室内空間ページ。",
+  "add_stylesheet" => ["css/private.css"],
+ "add_script" =>  ["js/private.js"],
+  "canonical_page_id" => "private", 
+  "logo_text" => "室内空間｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー古川橋｜古川橋 新築分譲マンション｜トップ</a></li>
+      <li><p>室内空間</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+  
+  <section class="content clearfix">
+  <div class="slider">
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img01.jpg" alt="">
+        <p class="caption">Living Dining</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img02.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img03.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img04.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img05.jpg" alt="">
+        <p class="caption">BEDROOM1</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img06.jpg" alt="">
+        <p class="caption">BEDROOM2</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img07.jpg" alt="">
+        <p class="caption">BALCONY</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img08.jpg" alt="">
+        <p class="caption">BATHROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img09.jpg" alt="">
+        <p class="caption">POWDER ROOM</p>
+      </div>
+    </div>
+    <div class="slick-img">
+      <div class="image">
+        <img src="./images/private/img10.jpg" alt="">
+        <p class="caption">ENTRANCE</p>
+      </div>
+    </div>
+  </div>
+   <div class="thumbnail">
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img01.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img02.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img03.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>   
+     </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img04.jpg" alt="">
+        <p class="caption">LIVING DINING ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img05.jpg" alt="">
+        <p class="caption">BEDROOM1</p>
+      </div>    
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img06.jpg" alt="">
+        <p class="caption">BEDROOM2</p>
+      </div>    
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img07.jpg" alt="">
+        <p class="caption">BALCONY</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img08.jpg" alt="">
+        <p class="caption">BATHROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img09.jpg" alt="">
+        <p class="caption">POWDER ROOM</p>
+      </div>
+    </div>
+    <div class="thumbnail-img">
+      <div class="image">
+        <img src="./images/private/img10.jpg" alt="">
+        <p class="caption">ENTRANCE</p>
+      </div>
+    </div>
+  </div>
+  </section>
+  <!-- content -->
+
+  <div class="annotation-area">
+  <p class="page-annotation js-scroll wrap clearfix">※掲載の写真はモデルルーム（I-2）を撮影（2024年12月）したもので、家具・調度品等オプション仕様は販売価格に含まれておりません。<br>※モデルルームの仕様等は、施工上の都合・改良等により一部変更になる場合があります。<br>※設備・仕様はタイプにより異なります。<br>※バルコニーの家具・植栽・調度品等の配置はイメージです。実際に配置される際には、安全等に十分配慮ください。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
