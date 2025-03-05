0b4048d9a7b487e548d11a7b61a7f1971d84763f
橋本真奈美 <manako.075h@gmail.com>
2025-03-05 18:15:01 +0900

    no message

---
+++ develop/public01.php
@@ -0,0 +1,93 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "public01",
+  "gnav_set" => "public01",
+  "page_class" => "public01",
+  "url" => "public01.html",
+  "title" => "迎賓空間｜",
+  "keywords" => "迎賓空間,",
+  "description" => "迎賓空間ページ。",
+  "add_stylesheet" => ["css/public01.css"],
+  "add_script" =>  [""],
+  "canonical_page_id" => "public01", 
+  "logo_text" => "迎賓空間｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー古川橋｜古川橋 新築分譲マンション｜トップ</a></li>
+      <li><p>迎賓空間</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/public01/mv.jpg" alt="" class=""><p class="annotation white">グランドエントランスホール完成予想図</p></div>
+  </header><!-- main-visual -->
+
+  
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <ul class="tab-btn">
+        <li>
+          <a href="./public01.html">迎賓空間</a>
+        </li>
+        <li>
+          <a href="./public02.html">共用空間</a>
+        </li>
+      </ul>
+      <p class="en-tit">Guest Space</p>
+      <p class="tit">迎賓空間</p>
+    </div><!-- inner -->
+  </section><!-- intro -->
+
+
+
+  <div class="images">
+    <div class="image">
+    <img src="./images/public01/img01.jpg" alt="" class="">
+    <p class="annotation white">グランドエントランスホール完成予想図</p>
+    </div>
+    <div class="texts">
+      <p class="en-tit">GRAND ENTRANCE HALL</p>
+      <p class="txt">２層吹抜の贅沢な開放感。</p>
+      <p class="lead">天井高約5.8m・約215㎡の２層吹抜空間が、<br class="pc">贅沢なゆとりと開放感を感じさせるグランドエントランスホール。<br class="pc">大きなガラスが特徴的なホールの中央には、<br class="pc">ゆったりとくつろげるソファスペースをご用意しました。</p>
+    </div>
+
+    <div class="image">
+    <img src="./images/public01/img02.jpg" alt="" class="">
+    <p class="annotation white">エレベーターホール完成予想図</p>
+    </div>
+    <div class="texts">
+      <p class="en-tit">ELEVATOR HALL</p>
+      <p class="txt">格調高い木の温もり。</p>
+      <p class="lead">計６基を配したエレベータホールは、<br class="pc">グランドエントランスホール同様、木目調のデザインを連続させ、<br class="pc">私邸へと続く動線に格調高い温もりを演出しています。</p>
+    </div>
+
+    <div class="image">
+    <img src="./images/public01/img03.jpg" alt="" class="">
+    <p class="annotation white">コーチエントランス完成予想図</p>
+    </div>
+    <div class="texts">
+      <p class="en-tit">COACH ENTRANCE</p>
+      <p class="txt">優雅に誘う、壮麗な車寄せ。</p>
+      <p class="lead">グランドエントランスホールに直結する建物北側に、<br class="pc">エレガントな雰囲気が漂うホテルライクなコーチエントランスを配置。<br class="pc">車でのお出かけとお帰りのシーンに優雅なひとときを演出します。</p>
+    </div>
+  </div><!-- images -->
+
+
+
+<div class="annotation-area">
+  <p class="page-annotation js-scroll wrap clearfix">※掲載のグランドエントランスホール・エレベーターホール完成予想図は計画段階の図面を基に描き起こしたもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>※掲載のコーチエントランス完成予想図は計画段階の図面を基に描き起こしたものに人物・車等をCG合成したもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>※外構・植栽計画、家具・調度品等の形状・色等は今後変更される場合がありますので、あらかじめご了承ください。<br>※敷地内の樹木等はある程度生育した後をイメージして描いたものです。また、葉や花の色合い、樹形等はイメージであり実際とは異なります。なお、植栽計画は変更される場合がありますので、あらかじめご了承ください。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
---
+++ develop/public02.php
@@ -0,0 +1,229 @@
+<!doctype html><?php
+ $page_settings = array(
+  "directory_pos" => "./",
+  "page_id" => "public02",
+  "gnav_set" => "public02",
+  "page_class" => "public02",
+  "url" => "public02.html",
+  "title" => "共用空間｜",
+  "keywords" => "共用空間,",
+  "description" => "共用空間ページ。",
+  "add_stylesheet" => ["css/public02.css"],
+  "add_script" =>  [""],
+  "canonical_page_id" => "public02", 
+  "logo_text" => "共用空間｜"
+);
+?>
+
+<?php require(INCLUDES."header.php"); ?>
+
+<main>
+	<!-- パンくずリスト -->
+  <div class="breadcrumb sp-hide">
+    <ul>
+      <li><a href="./index.html">シティタワー古川橋｜古川橋 新築分譲マンション｜トップ</a></li>
+      <li><p>共用空間</p></li>
+    </ul>
+  </div>
+	<!--// パンくずリスト -->
+
+  <header class="main-visual clearfix">
+    <!-- <h2 class="page-lead gothic">Hello New World.</h2> -->
+   <div class="image"><img src="./images/public02/mv.jpg" alt="" class=""><p class="annotation white">パーティルーム完成予想図</p></div>
+  </header><!-- main-visual -->
+
+  
+  <section class="intro sections clearfix">
+    <div class="inner">
+      <ul class="tab-btn">
+        <li>
+          <a href="./public01.html">迎賓空間</a>
+        </li>
+        <li>
+          <a href="./public02.html">共用空間</a>
+        </li>
+      </ul>
+      <p class="en-tit">Common  Space</p>
+      <p class="tit">共用空間</p>
+    </div><!-- inner -->
+  </section><!-- intro -->
+
+
+
+  <div class="images">
+    <div class="image">
+    <img src="./images/public02/img01.jpg" alt="" class="">
+    <p class="annotation">テレワークラウンジ完成予想図</p>
+    </div>
+    <div class="texts">
+      <p class="en-tit">PUBLIC SPACE</p>
+      <p class="txt">［3F］ テレワークラウンジ</p>
+      <p class="lead">仕事や勉強に集中できる快適なサードプレイス。<br class="pc">オープンスペースを機能的に配置し、心地良い広がりを感じられる空間となっています。</p>
+    </div>
+  </div><!-- images -->
+
+
+
+  <div class="sections floor clearfix">
+    <p class="tit">FLOOR INTRODUCTION</p>
+    <div class="inner">
+    <p class="lead">大規模タワーレジデンスだからこそ実現できる多彩なパブリックスペース。</p>
+    </div><!-- inner -->
+    <div class="image img01">
+    <img src="./images/public02/floor01.png" alt="" class="">
+    <p class="annotation">■フロア概念図</p>
+    </div>
+  </div><!-- floor -->
+
+
+
+  <div class="sections features clearfix">
+    <div class="inner">
+    <ul class="lists list01">
+      <li>
+        <div class="texts">
+          <p class="name">［3F］ パーティルーム</p>
+          <p class="info">明るく開放的な空間にこだわったパーティルーム。気の置けない仲間たちと大人の社交を楽しむ場としてはもちろん、子育て家族が集まって手料理を囲みながら盛り上がることもできます。</p>
+        </div>
+      </li>
+      <li>
+        <div class="image">
+        <img src="./images/public02/features01.jpg" alt="" class="">
+        <p class="annotation white">パーティルーム完成予想図</p>
+        </div>
+      </li>
+    </ul>
+    <ul class="lists list02">
+      <li>
+        <div class="image">
+        <img src="./images/public02/features02.jpg" alt="" class="">
+        </div>
+      </li>
+      <li>
+        <div class="texts">
+          <p class="name">［1F］ キッズパーク</p>
+          <p class="info">新しい街区の賑わい拠点として、地域の方々にもご利用いただける子育て家族のコミュニティスペースをご用意しました。</p>
+        </div>
+      </li>
+    </ul>
+    </div><!-- inner -->
+
+    <ul class="lists list03">
+      <li>
+        <div class="image">
+        <img src="./images/public02/features03.jpg" alt="" class="">
+        </div>
+      </li>
+      <li>
+        <div class="texts">
+          <p class="name">［30F］ スカイラウンジ</p>
+          <p class="info">タワーレジデンスならではの、高層階でゆったりとくつろげるスカイラウンジ。<br class="pc">上質なソファやテーブルもご用意し、非日常の空間で語らいや交流を楽しめる設えとしています。</p>
+        </div>
+      </li>
+    </ul>
+
+    <div class="inner">
+    <ul class="lists list01">
+      <li>
+        <div class="texts">
+          <p class="name">［3F］ フィットネスルーム</p>
+          <p class="info">毎日を健康的に過ごせるよう、天候を気にせず運動ができるフィットネスルームをご用意。</p>
+        </div>
+      </li>
+      <li>
+        <div class="image">
+        <img src="./images/public02/features04.jpg" alt="" class="">
+        <p class="annotation white">フィットネスルーム完成予想図</p>
+        </div>
+      </li>
+    </ul>
+    <ul class="lists list02">
+      <li>
+        <div class="image">
+        <img src="./images/public02/features05.jpg" alt="" class="">
+        <p class="annotation white">image photo</p>
+        </div>
+      </li>
+      <li>
+        <div class="texts">
+          <p class="name">［B1F］ スタジオ</p>
+          <p class="info">楽器演奏に対応した防音仕様のスタジオ。外出することなく利用できるので、重たい楽器を運ぶ負担も軽減できます。</p>
+        </div>
+      </li>
+    </ul>
+    </div><!-- inner -->
+
+    <div class="img06">
+    <div class="image">
+      <img src="./images/public02/features06.jpg" alt="" class="">
+      <p class="annotation white">ゲストルーム‐コンフォート‐完成予想図</p>
+    </div>
+    <div class="inner">
+    <p class="name">［3F］ ゲストルーム‐コンフォート‐</p>
+    </div>
+    </div><!-- img06 -->
+
+    <div class="inner">
+    <ul class="lists list04">
+      <li>
+        <div class="texts">
+          <p class="info">都会的でスタイリッシュなデザインの居室と、和モダンテイストでシンプルな居室。趣向の異なる２つのゲストルームが、大切なお客様をお迎えします。</p>
+        </div>
+      </li>
+      <li>
+        <div class="image">
+        <img src="./images/public02/features07.jpg" alt="" class="">
+        <p class="annotation white">ゲストルーム‐和・モダン‐完成予想図</p>
+        </div>
+        <p class="name">［3F］ ゲストルーム‐和・モダン‐</p>
+      </li>
+    </ul>
+    </div><!-- inner -->
+  </div><!-- features -->
+
+
+
+  <div class="sections corridor clearfix">
+    <div class="image">
+      <img src="./images/public02/features08.jpg" alt="" class="">
+      <p class="annotation white">内廊下完成予想図</p>
+    </div>
+    <div class="inner">
+      <p class="">INNER CORRIDOR</p>
+      <p class="">ホテルライクな内廊下</p>
+      <p class="">住まいへと心地よく導けるよう、<br>エレベーターホールから各邸まではプライバシー性や防犯性を高めたホテルライクな内廊下設計を採用。<br>空調も設置されており、外気に触れずにアプローチできます。 </p>
+      <div class="white-bg">
+        <p class="">内廊下の4つのメリット</p>
+        <ul>
+          <li>
+            <p class="">1. プライバシー性</p>
+            <p class="">外部の視線を遮りプライバシーに配慮。</p>
+          </li>
+          <li>
+            <p class="">2. 快適性</p>
+            <p class="">冷暖房を設置しているので夏の暑さ、冬の寒さを低減。 </p>
+          </li>
+          <li>
+            <p class="">3. 防犯性</p>
+            <p class="">外部からの不正侵入の可能性が低く、防犯性を高める。 </p>
+          </li>
+          <li>
+            <p class="">4. 静謐性</p>
+            <p class="">床がタイルカーペット仕上げの為、足音が響きにくい。</p>
+          </li>
+        </ul>
+        <p class="mini">※内廊下等の空調設備は季節及び時間帯限定運転のため常時稼働しておりません。</p>
+      </div>
+      </div><!-- inner -->
+  </div><!-- corridor -->
+
+
+
+
+<div class="annotation-area">
+  <p class="page-annotation js-scroll wrap clearfix">※掲載のパーティルーム・テレワークラウンジ・フィットネスルーム完成予想図は計画段階の図面を基に描き起こしたものに人物等をCG合成したもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>※掲載のスカイラウンジ・ゲストルーム・内廊下完成予想図は計画段階の図面を基に描き起こしたもので、実際とは多少異なります。また、形状の細部および設備機器等については省略しております。<br>※家具・調度品等の形状・色等は今後変更される場合がありますので、あらかじめご了承ください。<br>※スタジオ・パーティルーム・各ゲストルームのご利用は有料となります。内容につきましては、変更になる場合があります。その他利用条件等詳しくは係員にお尋ねください。<br>※掲載のフロア概念図は、計画段階の図面を基に描き起こしたもので、形状・色等は実際とは多少異なります。また、一部敷地外の道路等を合わせて着彩しています。</p></div>
+</main>
+
+<?php require(INCLUDES."footer.php"); ?>
+</body>
+</html>
